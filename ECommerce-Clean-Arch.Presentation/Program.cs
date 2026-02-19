c006ba1ecf9fb72722ae53112941887c82dfd054
Marwan <marwan-walid1@hotmail.com>
2026-02-19 17:29:05 +0200

    Added .env successfuly

--- ECommerce-Clean-Arch.Presentation/Program.cs
+++ ECommerce-Clean-Arch.Presentation/Program.cs
@@ -8,6 +8,7 @@ class Program
 {
     public static void Main(string[] args)
     {
+        DotNetEnv.Env.Load();
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
@@ -15,7 +16,6 @@ class Program
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         builder.Services.AddControllers();
-        builder.Configuration.AddEnvironmentVariables();
         {
             builder.Services
                 .AddApplication()
