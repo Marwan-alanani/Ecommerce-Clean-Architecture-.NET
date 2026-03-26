b4de298135a2afcdb5a7ec6777c5c3c8d7d4b3e1
Marwan <marwan-walid1@hotmail.com>
2026-03-27 01:36:44 +0200

    Wrote two unit tests

--- tests/Application.UnitTests/Categories/CreateCategoryTests.cs
+++ tests/Application.UnitTests/Categories/CreateCategoryTests.cs
@@ -1,9 +1,68 @@
+using ECommerce_Clean_Arch.Application.Abstractions.Persistence;
+using ECommerce_Clean_Arch.Application.Categories.Commands.Create;
+using ECommerce_Clean_Arch.Domain.Categories;
+
+using FluentAssertions;
+
+using Moq;
+using Moq.EntityFrameworkCore;
+
+using SharedKernel.Errors;
+
 namespace Application.UnitTests.Categories;
 
 public class CreateCategoryCommandHandlerTests
 {
-    [Fact]
-    public void Handle_Should_ReturnCategoryId_WhenNameIsUnique()
+    private readonly Mock<IApplicationDbContext> _context;
+    private readonly CreateCategoryCommandHandler _handler;
+
+    public CreateCategoryCommandHandlerTests()
     {
+        _context = new();
+        _handler = new(_context.Object);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnCategoryId_WhenNameIsUnique()
+    {
+        // Arrange
+        var categories = new List<Category>();
+
+        _context
+            .Setup(c => c.Categories)
+            .ReturnsDbSet(categories); // ← wire it up to your context mock
+
+        var command = new CreateCategoryCommand("Category");
+
+        // Act
+        var result = await _handler.Handle(
+            command,
+            CancellationToken.None);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().NotBeNull();
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnFailureResult_WhenNameIsNotUnique()
+    {
+        // Arrange
+        var categories = new List<Category>() { Category.Create("Category") };
+
+        _context
+            .Setup(c => c.Categories)
+            .ReturnsDbSet(categories); // ← wire it up to your context mock
+
+        var command = new CreateCategoryCommand("Category");
+
+        // Act
+        var result = await _handler.Handle(
+            command,
+            CancellationToken.None);
+
+        // Assert
+        result.IsFailure.Should().BeTrue();
+        result.Error.Type.Should().Be(ErrorType.Conflict);
     }
 }
